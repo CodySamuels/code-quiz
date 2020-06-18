e129c3e486adb978a102b6c15f269f75f1cc873e
Cody Samuels <twowoodenspoons@gmail.com>
2020-06-17 18:23:08 -0700

    Added score multiplier

--- script.js
+++ script.js
@@ -19,12 +19,9 @@ var userScore = 0;
 var questionCounter = 0;
 var correctAnswer = "";
 var usernameValue = "";
-var userArray = JSON.parse(localStorage.getItem("user")) || []; 
-
-// DISPLAY VARIABLES ON RUN. WILL BE PLACED IN HTML BY FINAL DRAFT.
-formEl.style.display = "none";
-scoreBoardEl.style.display = "none";
-sideBarBody.style.display = "none";
+var userArray = JSON.parse(localStorage.getItem("user")) || [];
+var scoreMultiplier = 0;
+var timerInterval = setInterval(startTimer, 1000);
 
 // AN ARRAY WITH AN OBJECTS IN IT. FOR EASILY DETERMINING CORRECTNESS.
 var questionsArray = [{
@@ -54,10 +51,10 @@ startButtonEl.addEventListener("click", runQuiz)
 
 // THIS FUNCTION STARTS THE QUIZ.
 function runQuiz() {
-startButtonEl.style.display = "none";
-scoreBoardEl.style.display = "none";
-sideBarBody.style.display = "";
-formEl.style.display = "none";
+  startButtonEl.style.display = "none";
+  scoreBoardEl.style.display = "none";
+  sideBarBody.style.display = "";
+  formEl.style.display = "none";
   startTimer()
   nextQuestion()
 }
@@ -75,30 +72,32 @@ function nextQuestion() {
 function updateScore(add5) {
   userScore += add5;
   scoreEl.textContent = userScore + " Points";
-  if (questionCounter >= questionsArray.length -1) {
+  if (questionCounter >= questionsArray.length - 1) {
     finalScreen();
   } else {
-  questionCounter++;
-  nextQuestion();
+    questionCounter++;
+    nextQuestion();
   }
 }
 
 // COUNTDOWN TIMER CODE.
 function startTimer() {
-  var timerInterval = setInterval(function () {
-    secondsLeft--;
-    timerEl.textContent = secondsLeft + " Seconds";
+  secondsLeft--;
+  timerEl.textContent = secondsLeft + " Seconds";
 
-    if (secondsLeft <= 0) {
-      clearInterval(timerInterval);
-      finalScreen();
-    }
-
-  }, 1000);
+  if (secondsLeft <= 0) {
+    clearInterval(timerInterval);
+    finalScreen();
+  }
 }
+
 
 // THIS FUNCTION IS FOR THE GAMEOVER SCREEN.
 function finalScreen() {
+  clearInterval(timerInterval);
+  scoreMultiplier = secondsLeft / 10;
+  userScore = Math.floor(userScore * scoreMultiplier);
+  scoreEl.textContent = userScore + " Points";
   formEl.style.display = "";
   h2El.textContent = "Enter your username!"
   p1El.textContent = ""
@@ -108,8 +107,8 @@ function finalScreen() {
   formEl.addEventListener("submit", function (event) {
     event.preventDefault();
     var userInfo = {
-      Username: inputEl.value,
-      Score: userScore
+      username: inputEl.value,
+      score: userScore
     };
     userArray.push(userInfo);
     localStorage.setItem("user", JSON.stringify(userArray));
@@ -123,7 +122,7 @@ function clearScreen() {
   startButtonEl.style.display = "none";
   sideBarBody.style.display = "none";
   scoreBoardEl.style.display = "none";
-  formEl.style.display="none";
+  formEl.style.display = "none";
 }
 
 // THIS IS THE SCOREBOARD SCREEN. THIS NEEDS A LOT OF WORK. 
@@ -134,16 +133,16 @@ function scoreBoardScreen() {
   p3El.textContent = "";
   p4El.textContent = "";
   scoreBoardEl.style.display = "";
-  formEl.style.display="none";
-  sideBarBody.style.display="none";
-  startButtonEl.style.display="none";
+  formEl.style.display = "none";
+  sideBarBody.style.display = "none";
+  startButtonEl.style.display = "none";
   for (let i = 0; i <= userArray.length; i++) {
     trEl = document.createElement("tr");
     thEl = document.createElement("th");
     td1El = document.createElement("td");
     td2El = document.createElement("td");
-    td1El.textContent = userArray[i].Username;
-    td2El.textContent = userArray[i].Score;
+    td1El.textContent = userArray[i].username;
+    td2El.textContent = userArray[i].score;
     scoreBoardEl.append(trEl, thEl, td1El, td2El);
   }
 }
@@ -154,7 +153,7 @@ p1El.addEventListener("click", function (event) {
     updateScore(5);
   } else {
     p1El.textContent = "Incorrect."
-    secondsLeft = secondsLeft -5;
+    secondsLeft = secondsLeft - 5;
   }
 });
 
@@ -163,7 +162,7 @@ p2El.addEventListener("click", function (event) {
     updateScore(5);
   } else {
     p2El.textContent = "Incorrect."
-    secondsLeft = secondsLeft -5;
+    secondsLeft = secondsLeft - 5;
   }
 });
 
@@ -172,7 +171,7 @@ p3El.addEventListener("click", function (event) {
     updateScore(5);
   } else {
     p3El.textContent = "Incorrect."
-    secondsLeft = secondsLeft -5;
+    secondsLeft = secondsLeft - 5;
   }
 });
 
@@ -181,6 +180,6 @@ p4El.addEventListener("click", function (event) {
     updateScore(5);
   } else {
     p4El.textContent = "Incorrect."
-    secondsLeft = secondsLeft -5;
+    secondsLeft = secondsLeft - 5;
   }
 })
